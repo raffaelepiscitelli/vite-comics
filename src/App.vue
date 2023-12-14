bada041f9cdb416f6a18cd190c7eab698f1d0cbc
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-12-14 22:09:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+  </div>
+</template>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+<style lang="scss">
+@use './styles/general.scss' as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,77 @@
+<template lang="">
+    <header>
+        <div class="header-container">
+            <section class=logo>
+                <img src="../assets/imgs/dc-logo.png" alt="">
+            </section>
+            <section class="navbar">
+                <ul>
+                    <li>
+                        Characters
+                    </li>
+                    <li>
+                        Comics
+                    </li>
+                    <li>
+                        Movies
+                    </li>
+                    <li>
+                        Tv
+                    </li>
+                    <li>
+                        Games
+                    </li>
+                    <li>
+                        Collectibles
+                    </li>
+                    <li>
+                        Videos
+                    </li>
+                    <li>
+                        Fans
+                    </li>
+                    <li>
+                        News
+                    </li>
+                    <li>
+                        Shop
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+    .header-container {
+        @include flex (
+            row, space-around, center
+        );
+        margin: 1rem 6rem 0;
+        section.logo img {
+            height: 4rem;
+        };
+
+        ul {
+            @include flex (
+                row, center, center
+            );
+            list-style-type: none;
+        }
+
+        li {
+            padding: 0.8rem;
+            font-size: 0.6rem;
+            text-transform: uppercase;
+            font-weight: bolder;
+        }
+    }
+</style>
